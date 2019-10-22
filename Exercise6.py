d1f7d26b9d9c001a945c5a91ca859c16949e2ecd
Ryan Li <ryan.lichangrui@gmail.com>
2019-10-22 13:51:47 -0400

    completed Exercise 6

--- Exercise6.py
+++ Exercise6.py
@@ -3,25 +3,30 @@
 # Purpose:     Display my understanding of functions
 #
 # Author:      767571
-# Created:     Oct-21-19
-# Updated:     Oct-21-19
+# Created:     Oct-22-19
+# Updated:     Oct-22-19
 #-----------------------------------------------------------------------------
 
 
 def convertCToF(temperature):
     if -100 <= temperature <= 100:
         return int(temperature * (9/5)) + 32
+    return 'Unacceptable input values.'
 
 
 def convertFToC(temperature):
     if -100 <= temperature <= 100:
         return int(temperature-32) * (5/9)
+    return 'Unacceptable input values.'
 
 
 def hypotenuse(a, b):
-    return float((a**2 + b**2)**1/2)
+    if a <= 0 or b <= 0:
+        return 'Unacceptable input values.'
+    return float((a**2 + b**2)**(0.5))
 
 
 def milesToKm(distance):
     if distance > 0:
-        return float(round(distance*1.609, 2))
+        return float(round(distance*1.60934, 2))
+    return 'Unacceptable input values.'
